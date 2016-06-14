80c24c08000fb8e5cd36fb385ea358700db4482c
Nick Perez <nperez0111@gmail.com>
2016-06-14 16:46:25 -0400

    More Tests

--- test/index.js
+++ test/index.js
@@ -1,2 +1,22 @@
 var isCyclical = require( './../index.js' );
+
 console.assert( isCyclical( [ 1, 2, 3, 1, 2, 3, 1, 2, 3 ], 1 ) );
+
+console.assert( isCyclical( [ 1, 2, 1, 2, 1, 2 ], 1 ) );
+
+console.assert( isCyclical( [ 'a', 'b', 'c', 'a', 'b', 'c' ], 'a' ) );
+
+console.assert( isCyclical( [ 1, 2, 3, 4, 1, 2, 3, 4 ].map( function ( c ) {
+    return [ c ];
+} ), [ 1 ] ) );
+console.assert( isCyclical( [ {
+    a: "ok"
+}, {
+    d: "no"
+}, {
+    a: "ok"
+}, {
+    d: "no"
+} ], {
+    a: "ok"
+} ) );
